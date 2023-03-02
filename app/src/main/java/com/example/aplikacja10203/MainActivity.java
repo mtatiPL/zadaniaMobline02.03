a5c97ac69a93d1def671fb97c481d90bd6970042
mtatiPL <mateusz-biernat@tlen.pl>
2023-03-02 14:21:50 +0100

    licznik

--- app/src/main/java/com/example/aplikacja10203/MainActivity.java
+++ app/src/main/java/com/example/aplikacja10203/MainActivity.java
@@ -2,13 +2,52 @@ package com.example.aplikacja10203;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int sekundy=0;
+    boolean running=true;
+    Button button;
+    TextView textView;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.textViewTimerUp);
+        button=findViewById(R.id.button);
+       final Handler handler=new Handler();
+       handler.post(new Runnable() {
+           @Override
+           public void run() {
+               if(running) {
+                   sekundy++;
+                   textView.setText(Integer.toString(sekundy));
+               }
+                   handler.postDelayed(this, 1000);
+
+
+
+           }
+       });
+       button.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               if(running){
+                   running=false;
+                   button.setText("dalej");
+               }
+               else{
+                   running=true;
+                   button.setText("Pauza");
+               }
+           }
+       });
     }
 }
